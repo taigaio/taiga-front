d3a97abab1e67ffee018371d1481418c3c3eec8e
Juanfran <juanfran.alcantara@kaleidos.net>
2015-05-27 09:00:09 +0200

    travis support

--- karma.conf.js
+++ karma.conf.js
@@ -2,7 +2,7 @@
 // Generated on Wed Apr 15 2015 09:44:14 GMT+0200 (CEST)
 
 module.exports = function(config) {
-  config.set({
+  var configuration = {
 
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
@@ -78,9 +78,22 @@ module.exports = function(config) {
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['Chrome'],
 
+    customLaunchers: {
+      Chrome_travis_ci: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      }
+    },
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
     singleRun: false
-  });
+  };
+
+  if(process.env.TRAVIS){
+    configuration.browsers = ['Chrome_travis_ci'];
+    configuration.singleRun = true;
+  }
+
+  config.set(configuration);
 };
