f6f251d2dc366adbd876685bbd04e95e8cf98627
Alejandro Alonso <alejandroalonsofernandez@gmail.com>
2016-09-29 09:18:13 +0200

    Fixing epics e2e suite

--- e2e/helpers/epic-detail-helper.js
+++ e2e/helpers/epic-detail-helper.js
@@ -32,6 +32,7 @@ helper.colorEditor = function() {
 
 helper.relatedUserstories = function() {
     let el = $('tg-related-userstories');
+    let lightboxCreateRelatedUserStories = el.$(".lightbox-create-related-user-stories");
 
     let obj = {
         el: el,
@@ -42,7 +43,7 @@ helper.relatedUserstories = function() {
             el.$(".e2e-single-creation-label").click();
             el.$(".e2e-new-userstory-input-text").sendKeys(subject);
             el.$(".e2e-create-userstory-button").click();
-            await browser.waitForAngular();
+            await utils.lightbox.close(lightboxCreateRelatedUserStories);
         },
 
         createNewUserStories: async function(subject) {
@@ -51,17 +52,16 @@ helper.relatedUserstories = function() {
             el.$(".e2e-bulk-creation-label").click();
             el.$(".e2e-new-userstories-input-textarea").sendKeys(subject);
             el.$(".e2e-create-userstory-button").click();
-            await browser.waitForAngular();
+            await utils.lightbox.close(lightboxCreateRelatedUserStories);
         },
 
         selectFirstRelatedUserstory: async function() {
             el.$(".e2e-add-userstory-button").click();
             el.$(".e2e-existing-user-story-label").click();
             el.$(".e2e-filter-userstories-input").click().sendKeys("#1");
-            await browser.waitForAngular();
             el.$$(".e2e-userstories-select option").get(1).click()
             el.$(".e2e-select-related-userstory-button").click();
-            await browser.waitForAngular();
+            await utils.lightbox.close(lightboxCreateRelatedUserStories);
         },
 
         deleteFirstRelatedUserstory: async function() {
