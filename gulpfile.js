1338cb55dbca7a9eee989862ba69c96db5639e5a
Juanfran <juanfran.ag@gmail.com>
2020-09-09 08:14:32 +0200

    feat(core): add elements to the repo

--- gulpfile.js
+++ gulpfile.js
@@ -196,8 +196,8 @@ paths.libs = [
     paths.app + "js/markdown-it-lazy-headers.js"
 ];
 
-if (fs.existsSync(`${paths.modules}elements.js`)) {
-    paths.libs.push(`${paths.modules}elements.js`);
+if (fs.existsSync(`./elements.js`)) {
+    paths.libs.push(`./elements.js`);
 }
 
 paths.libs.forEach(function(file) {
