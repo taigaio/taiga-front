46d4d7d79b85d05f7177e2e3f7ac02ea976da0a3
Yamila Moreno <yamila.ms@gmail.com>
2020-10-06 19:15:34 +0200

    Add Gitlab Auth Contrib to docker

--- docker/config_env_subst.sh
+++ docker/config_env_subst.sh
@@ -1,2 +1,5 @@
-envsubst < /usr/share/nginx/html/conf.json.template \
-         > /usr/share/nginx/html/conf.json
+FILE=/usr/share/nginx/html/conf.json
+if [ ! -f "$FILE" ]; then
+    envsubst < /usr/share/nginx/html/conf.json.template \
+             > /usr/share/nginx/html/conf.json
+fi
